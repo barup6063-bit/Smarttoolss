96f2caf6c31e168227c2f23a90e03ca03ca115e6
barup6063-bit <barup6063@gmail.com>
2026-05-06 02:20:20 +0700

    Update video_tool.dart

--- lib/video_tool.dart
+++ lib/video_tool.dart
@@ -1,32 +1,48 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoTool extends StatefulWidget {
-  @override
-  _VideoToolState createState() => _VideoToolState();
+  @override _VideoToolState createState() => _VideoToolState();
 }
 
 class _VideoToolState extends State<VideoTool> {
-  TextEditingController c = TextEditingController();
-  String url = "";
+  File? file;
+  VideoPlayerController? controller;
 
-  void getThumb() {
-    String id = c.text.split("v=")[1];
-    setState(() {
-      url = "https://img.youtube.com/vi/$id/0.jpg";
-    });
+  Future pick() async {
+    final x = await ImagePicker().pickVideo(source: ImageSource.gallery);
+    if (x != null) {
+      file = File(x.path);
+      controller = VideoPlayerController.file(file!)
+        ..initialize().then((_) => setState(() {}));
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Video Tool")),
-      body: Column(
-        children: [
-          TextField(controller: c),
-          ElevatedButton(onPressed: getThumb, child: Text("Get Thumbnail")),
-          if (url != "") Image.network(url),
-        ],
-      ),
+      body: Column(children: [
+        ElevatedButton(onPressed: pick, child: Text("Pick Video")),
+        if (controller != null && controller!.value.isInitialized)
+          AspectRatio(
+            aspectRatio: controller!.value.aspectRatio,
+            child: VideoPlayer(controller!),
+          ),
+        if (controller != null)
+          ElevatedButton(
+            onPressed: () => controller!.value.isPlaying ? controller!.pause() : controller!.play(),
+            child: Text("Play/Pause"),
+          ),
+      ]),
     );
   }
 }
